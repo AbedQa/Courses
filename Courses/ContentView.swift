05cfdb60f2b244cc445bf7220bfcefdb07157bcc
Abdelrahman Qasim <abdelrahman.qasim@orange.com>
2022-01-28 15:19:25 +0200

    Initial Commit

---
+++ Courses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Courses
+//
+//  Created by AbdelrahmanQasim on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Courses/CoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesApp.swift
+//  Courses
+//
+//  Created by AbdelrahmanQasim on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct CoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
